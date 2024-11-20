5cc4cb00e8190dcd336f8b307a2a0a7770d83bba
dfsx <dfsx>
2024-11-20 17:04:26 +0800

    fix install script, and add test vector add scrpt.

--- pip-install.sh
+++ pip-install.sh
@@ -1,8 +1,10 @@
 set -e
 
-export LLVM_BUILD_DIR=~/llvm-project/build
+# export LLVM_BUILD_DIR=~/llvm-project/build
+# LLVM_INCLUDE_DIRS=$LLVM_BUILD_DIR/include \
+#   LLVM_LIBRARY_DIR=$LLVM_BUILD_DIR/lib \
+#   LLVM_SYSPATH=$LLVM_BUILD_DIR \
+#   pip install ./python
+
 export DEBUG=1
-LLVM_INCLUDE_DIRS=$LLVM_BUILD_DIR/include \
-  LLVM_LIBRARY_DIR=$LLVM_BUILD_DIR/lib \
-  LLVM_SYSPATH=$LLVM_BUILD_DIR \
-  pip install -e ./python
+pip install -e ./python
